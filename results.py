451a6b3876bae4b5d632accd3aaafdbec4ae2313
jakezachariahnixon <jake.zachariah.nixon@gmail.com>
2019-12-08 22:01:19 +0000

    Day 3 Part 1

--- results.py
+++ results.py
@@ -1,5 +1,6 @@
 from solutions.day1 import *
 from solutions.day2 import *
+from solutions.day3 import *
 
 def main():
     print("DAY 1:")
@@ -12,6 +13,11 @@ def main():
     print("\t\t" + str(day_two_part_one()))
     print("\tPART 2:")
     print("\t\t" + str(day_two_part_two()))
+    print("DAY 3:")
+    print("\tPART 1:")
+    print("\t\t" + str(day_three_part_one()))
+    print("\tPART 2:")
+    print("\t\t" + str(day_three_part_two()))
 
 if __name__ == '__main__':
     main()
